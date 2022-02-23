a595d43afe569425bbfa69b3f3ad953588c15353
WashFall <carl.christensen98@gmail.com>
2022-02-23 12:19:06 +0100

    few fixes

--- GameJam/Assets/Code/PlayerMovement/PlayerMovement.cs
+++ GameJam/Assets/Code/PlayerMovement/PlayerMovement.cs
@@ -1,17 +1,20 @@
-using UnityEngine.InputSystem;
+using System.Collections;
 using UnityEngine;
-using System;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    ContactPoint2D point;
     float jump = 13;
     float xMove = 0;
+    private float xJump;
     float speed = 13f;
+    float jumpValue;
     Movement movement;
     Vector2 movementVector;
     bool isGrounded = false;
     private bool jumping;
+    float jumpHeight;
 
     private void Start()
     {
@@ -24,23 +27,18 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         movementVector = movement.Player.Move.ReadValue<Vector2>();
-        float jumpValue = movement.Player.Jump.ReadValue<float>();
+        jumpValue = movement.Player.Jump.ReadValue<float>();
 
         Move();
 
-        if(isGrounded)
-            Jump(jumpValue);
+        if (isGrounded)
+            Jump();
 
     }
 
-    private void JumpRelease()
+    private void Jump()
     {
-
-    }
-
-    private void Jump(float jumpValue)
-    {
-        rb.AddForce(new Vector2(0, jump) * jumpValue, ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0, jump) * jumpValue, ForceMode2D.Impulse);
     }
 
     private void Move()
@@ -48,6 +46,7 @@ public class PlayerMovement : MonoBehaviour
         if (movement.Player.Move.inProgress)
         {
             xMove = movementVector.x;
+            xJump = xMove;
             speed = 10f;
             var mover = new Vector2(xMove, 0) * Time.fixedDeltaTime * speed;
             transform.position += (Vector3)mover;
@@ -55,29 +54,38 @@ public class PlayerMovement : MonoBehaviour
         else if (movementVector == Vector2.zero)
         {
             if (jumping)
-                speed = 8f;
+            {
+                var mover = new Vector2(xJump, 0) * Time.fixedDeltaTime * speed;
+                transform.position += (Vector3)mover;
+            }
             else
-                speed = Mathf.MoveTowards(speed, 0, 1.5f);
-            var mover = new Vector2(xMove, 0) * Time.fixedDeltaTime * speed;
-            transform.position += (Vector3)mover;
+            {
+                speed = Mathf.MoveTowards(speed, 0, 1f);
+                var mover = new Vector2(xMove, 0) * Time.fixedDeltaTime * speed;
+                transform.position += (Vector3)mover;
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        ContactPoint2D point = collision.GetContact(0);
+        point = collision.GetContact(0);
         if(point.point.y < transform.position.y)
         {
             isGrounded = true;
             jumping = false;
+            xJump = 0;
         }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        ContactPoint2D point = collision.GetContact(0);
+        point = collision.GetContact(0);
         if (point.point.y < transform.position.y)
+        {
             isGrounded = true;
+            jumping = false;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
