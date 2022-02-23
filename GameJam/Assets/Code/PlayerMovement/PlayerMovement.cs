49a38e6cadc0ec4ac61c188bac84a01e27ef9c53
WashFall <carl.christensen98@gmail.com>
2022-02-23 09:52:15 +0100

    asd

--- GameJam/Assets/Code/PlayerMovement/PlayerMovement.cs
+++ GameJam/Assets/Code/PlayerMovement/PlayerMovement.cs
@@ -7,8 +7,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     float jump = 13;
     float xMove = 0;
-    float awayForce;
     float speed = 13f;
+    float jumpValue;
     Movement movement;
     Vector2 movementVector;
     bool isGrounded = false;
@@ -25,19 +25,23 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         movementVector = movement.Player.Move.ReadValue<Vector2>();
-        float jumpValue = movement.Player.Jump.ReadValue<float>();
+        jumpValue = movement.Player.Jump.ReadValue<float>();
 
         Move();
 
         if(isGrounded)
-            Jump(jumpValue);
+            Jump();
 
     }
 
-    private void Jump(float jumpValue)
+    private void JumpRelease()
+    {
+
+    }
+
+    private void Jump()
     {
         rb.AddForce(new Vector2(0, jump) * jumpValue, ForceMode2D.Impulse);
-        rb.AddForce(new Vector2(awayForce, 0) * jumpValue, ForceMode2D.Impulse);
     }
 
     private void Move()
@@ -51,7 +55,10 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (movementVector == Vector2.zero)
         {
-            speed = Mathf.MoveTowards(speed, 0, 1.5f);
+            if (jumping)
+                speed = 8f;
+            else
+                speed = Mathf.MoveTowards(speed, 0, 1.5f);
             var mover = new Vector2(xMove, 0) * Time.fixedDeltaTime * speed;
             transform.position += (Vector3)mover;
         }
@@ -61,11 +68,22 @@ public class PlayerMovement : MonoBehaviour
     {
         ContactPoint2D point = collision.GetContact(0);
         if(point.point.y < transform.position.y)
+        {
+            isGrounded = true;
+            jumping = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        ContactPoint2D point = collision.GetContact(0);
+        if (point.point.y < transform.position.y)
             isGrounded = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
+        jumping = true;
     }
 }
